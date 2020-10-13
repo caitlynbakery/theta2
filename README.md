bdb344d1061799f0159cefafd5f28fad325740ed
Caitlyn <craigoda+caitlyn@gmail.com>
2020-10-13 07:45:28 -0700

    added image toggle feature

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# theta2
+# Theta App
 
-A new Flutter project.
+To use the Provider package, you first have to create a Provider class.
+```dart
+class MainResponseWindow with ChangeNotifier {
+  String _responseText = "";
+  String get responseText => _responseText;
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+  void updateResponseWindow(response) {
+    _responseText = response;
+    notifyListeners();
+  }
+}
+```
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
       create: (_) => MainResponseWindow(),
-    )
+    ),
+    ChangeNotifierProvider(create: (_) => MainRequestWindow(),),
+    ChangeNotifierProvider(create: (_) => PictureWindow(),)
   ], child: MyApp()));
 }
 
@@ -22,6 +24,26 @@ class MainResponseWindow with ChangeNotifier {
   }
 }
 
+class MainRequestWindow with ChangeNotifier {
+  String _requestText = "";
+  String get requestText => _requestText;
+
+  void updateRequestWindow(request){
+    _requestText = request;
+    notifyListeners();
+  }
+}
+
+class PictureWindow with ChangeNotifier {
+  bool _toggleImage = false;
+  bool get toggleImage => _toggleImage;
+
+  void toggleImageMethod(){
+    _toggleImage = !_toggleImage;
+    notifyListeners();
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -30,15 +52,16 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.teal),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Theta App'),
-        ),
-          
-          body:  SingleChildScrollView(
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
+          appBar: AppBar(
+            title: Text('Theta App'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+               
                 ToDoButton(),
                 MaterialButton(
                   onPressed: () async {
@@ -46,25 +69,62 @@ class MyApp extends StatelessWidget {
                         .get('https://jsonplaceholder.typicode.com/users');
                     var responseBody = response.body;
 
+                    var request = 'https://jsonplaceholder.typicode.com/users';
+
                     context
                         .read<MainResponseWindow>()
                         .updateResponseWindow(responseBody);
+
+                    context
+                      .read<MainRequestWindow>()
+                      .updateRequestWindow(request);
                   },
                   child: Text("Users"),
                 ),
+                MaterialButton(onPressed: (){
+                  context
+                    .read<PictureWindow>()
+                    .toggleImageMethod();
+                    
+                  print("Toggle image");
+                },child: Text("Toggle Image"),)
+              ]),
+              Expanded(
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 300,
+                        child: SingleChildScrollView(
+                child:
+                          Text(Provider.of<MainRequestWindow>(context).requestText),
+              ),
+                      ),
+                      SizedBox(width: 100),
+                      Container(
+                        width: 300,
+                        child: SingleChildScrollView(
+                child:
+                          Text(Provider.of<MainResponseWindow>(context).responseText),
+              ),
+                      ),
+                      Container(
+                        width: 300,
+                        child: Provider.of<PictureWindow>(context)._toggleImage ? ListView(children: [
+                          Image.network('https://picsum.photos/400/200?random=1'),
+                          SizedBox(height: 12),
+                           Image.network('https://picsum.photos/400/200?random=2'),
+SizedBox(height: 12),
+                            Image.network('https://picsum.photos/400/200?random=3'),
+SizedBox(height: 12),
+                             Image.network('https://picsum.photos/400/200?random=4'),
+SizedBox(height: 12),
+                              Image.network('https://picsum.photos/400/200?random=5'),
+                        ],) : Container())
 
-                 
-                      Text(Provider.of<MainResponseWindow>(context).responseText),
-                  
-            
-              ],
-            ), 
-          )
-            
-          
-          
-       
-      ),
+                    ],
+                  )),
+            ],
+          )),
     );
   }
 }
